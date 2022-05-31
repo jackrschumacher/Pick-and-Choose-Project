20f44ccc40e70ac7649fe3a3125677cf69db43f6
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-05-31 19:24:58 +0000

    Finish Sum

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
 class Main {
   public static void main(String[] args) {
+    int Sum;
     System.out.println("Print The Sum of 100*100");
+    Sum = 100 * 100;
+    System.out.println(Sum);
   }
 }

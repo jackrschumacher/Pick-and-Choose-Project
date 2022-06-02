a30948ca7b0cfe66e3bcf381a46bc6e2fe32e30e
Jack Schumacher <84467112+schujack-ctrl@users.noreply.github.com>
2022-06-02 18:46:42 +0000

    Add Division and Subraction

--- Main.java
+++ Main.java
@@ -10,5 +10,9 @@ class Main {
     System.out.println("Subtract 1000-1000");
     Sum = 1000-1000;
     System.out.println(Sum);
+    System.out.println("Divide 100/25");
+    Sum = 100/25;
+    System.out.println(Sum);
+    
   }
 }
